e4375333cd0cf888143e43fa9777407c95fe493e
Laura Heino <lhhein@gmail.com>
2021-05-04 16:48:58 +0200

    amend readme, add random quote to user page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { Container, Button } from 'react-bootstrap'
 import UserSettings from './UserSettings'
@@ -9,11 +10,30 @@ export default function Home(props) {
     const [showWelcome, setShowWelcome] = useState(true)
     const [showSettings, setShowSettings] = useState(false)
     const hours = new Date().getHours()
+    const [quote, setQuote] = useState(null)
+
 
     const handleShowSettings = () => {
         setShowWelcome(false)
         setShowSettings(true) 
     }
+
+    const getQuote = async () => {
+        const rand = Math.floor(Math.random() * 1500)
+        const result = await fetch('https://type.fit/api/quotes')
+        const data = await result.json()
+
+        let quote = {
+            author: data[rand].author,
+            quote: data[rand].text
+        }
+        setQuote(quote)
+    }   
+
+    
+    useEffect(() => {
+        getQuote(setQuote(quote))
+    }, [])
 
 
     return (
@@ -31,7 +51,7 @@ export default function Home(props) {
             <div className="w-100 welcome">
             { !props.user && <Redirect to={'/'} /> }
 
-                <Container className="m-5 border w-75 blur-bg">
+                <Container className="m-5 w-75 blur-bg">
                 {
                     showWelcome &&
                     <header className="user-frontpage-header">
@@ -54,8 +74,14 @@ export default function Home(props) {
                     {
                     showSettings && <UserSettings user={props.user}/>
                 }
-
+                { quote &&
+                    <div className="quote">
+                        <div className="font-italic">{quote.quote}</div>
+                    <div className="font-weight-bold">- { quote.author ? quote.author : 'anonymous'}</div>
+                    </div>
+                }
                 </Container>
+        
             </div>
             </div>
         </div>
